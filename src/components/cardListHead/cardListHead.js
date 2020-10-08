5bfaf447079816fab0675123c97d89641ba00cfb
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-10-08 23:47:12 +0800

    chore: 新增cardListHead component

---
+++ src/components/cardListHead/cardListHead.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import FlashApi from 'api/FlashApi';
+import Breadcrumb from 'components/breadcrumb/breadcrumb';
+
+class CardListHead extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.loadPath = this.loadPath.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
+
+    this.state = {
+      isLoading:this.props.listId?true:false,
+      path:[]
+    }
+  }
+  loadPath() {
+    FlashApi.getPath(this.props.listId).then(res=>{
+      this.setState({path:res.result,isLoading:false});
+    });
+  }
+
+
+  handleEnter(_id) {
+    this.props.onEnter(_id);
+  }
+
+  render () {
+    if(this.state.isLoading){
+      this.loadPath();
+      return (<div>loading...</div>)
+    }
+    const breadcrumbArray = [{title:'root',clickId:null},...this.state.path.map(item=>{
+      return {title:item.title,clickId:item._id};
+    })];
+    return (
+      <div>
+        <Breadcrumb onClickItem={this.handleEnter} itemArray={breadcrumbArray}/>
+      </div>
+    )
+  }
+}
+
+export default CardListHead;
