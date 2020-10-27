d747d073c77215f1ff8ca4b2d8c755c6c3493d26
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-10-27 20:45:50 +0800

    chore: 引入store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import * as actions from 'actions';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store'
+
+const store = configureStore();
+
+store.dispatch(actions.setNodeid('root'));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
