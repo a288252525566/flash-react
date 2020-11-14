5f5f1f8b1381a1ea561d000b6ea13eea64f54be5
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-11-14 11:18:47 +0800

    chore: styling breadcrumb

--- src/components/breadCrumb.js
+++ src/components/breadCrumb.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
+import styles from './breadCrumb.module.scss';
+import { ReactComponent as SeparatorIcon } from 'images/chevron-right.svg';
 
 const Breadcrumb = ({items})=>{
   const match = useRouteMatch();
-  return (
-    <div>
-      {items.map(item=> {
+  const Separator = <div className={styles.separator}><SeparatorIcon/></div>;
+  const [displayItems, setDisplayItems] = useState([]);
+  useEffect(()=>{
+    setDisplayItems([...items].reverse());
+  },[items]);
 
-        const linkto = item._id==='root' ? match.path : match.path+'/'+item._id;
-        return <Link key={item._id}  to={linkto}><button >{item.title}</button></Link>
-      })}
+
+
+  return (
+    <div className={styles.breadcrumb}>
+      <div className={styles.container}>
+        {displayItems.map((item, index)=> {
+          const linkto = item._id==='root' ? match.path : match.path+'/'+item._id;
+          const itemDom = <Link className={styles.item} key={item._id} data-_id={item._id} to={linkto}>{item.title}</Link>
+          if(index+1<items.length) return [itemDom,Separator];
+          else return itemDom;
+        })}
+      </div>
     </div>
   )
 }
