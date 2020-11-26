fab03f198a0ea853cdf1dc9bc09b34c21cb93c56
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-11-26 15:20:47 +0800

    fix: typo

--- src/components/listHead.js
+++ src/components/listHead.js
@@ -5,7 +5,7 @@ import * as styles from './listHead.module.scss';
 const ListHead = ({title, onClean})=>{
   return <div className={styles.listHead}>
     <h2 className={styles.title}>{title}</h2>
-    <div calssName={styles.buttons}>
+    <div className={styles.buttons}>
       <BroomIcon className={styles.button} title={'清除已完成項目'} onClick={onClean}/>
     </div>
   </div>
